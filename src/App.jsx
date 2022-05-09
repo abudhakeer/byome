fe4f2ee483c4aec7707dc71a418554ef6d68e17b
0xsuk <ikeuu53@yahoo.co.jp>
2022-05-09 21:35:07 +0900

    Open&Save file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createElement, Fragment, useEffect } from "react";
+import { useState, createElement, Fragment, useEffect, useRef } from "react";
 import "./App.css";
 import { unified } from "unified";
 import remarkParse from "remark-parse/lib";
@@ -10,6 +10,16 @@ import { EditorView } from "@codemirror/view";
 
 function App() {
   const [doc, setDoc] = useState("# Hello byome");
+  const [editorView, setEditorView] = useState(null);
+  const [fileName, setFileName] = useState(null);
+
+  const editorExtensions = [
+    EditorView.updateListener.of((update) => {
+      if (update.changes) {
+        setDoc(update.state.doc.toString());
+      }
+    }),
+  ];
 
   const md = unified()
     .use(remarkParse)
@@ -19,28 +29,69 @@ function App() {
     .processSync(doc).result;
 
   useEffect(() => {
+    window.addEventListener("beforeunload", (e) => {
+      e.returnValue = "";
+    });
+
     const startState = EditorState.create({
       doc,
-      extensions: [
-        EditorView.updateListener.of((update) => {
-          if (update.changes) {
-            setDoc(update.state.doc.toString());
-          }
-        }),
-      ],
+      extensions: editorExtensions,
     });
 
-    new EditorView({
-      state: startState,
-      parent: document.getElementById("editor"),
-    });
+    setEditorView(
+      new EditorView({
+        state: startState,
+        parent: document.getElementById("editor"),
+      })
+    );
   }, []);
 
+  const handleFileOpen = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (ev) => {
+      setDoc(ev.target.result);
+      const newState = EditorState.create({
+        doc: ev.target.result,
+        extensions: editorExtensions,
+      });
+      editorView.setState(newState);
+    };
+    reader.readAsText(file);
+
+    setFileName(file.name);
+  };
+
+  const downloadFile = () => {
+    const b64doc = btoa(doc);
+    console.log(b64doc, doc);
+    const a = document.createElement("a");
+    const e = new MouseEvent("click");
+
+    a.download = fileName ? fileName : "doc.md";
+    a.href = "data:text/plain;base64," + b64doc;
+    a.dispatchEvent(e);
+  };
+
+  const inputRef = useRef(null);
   return (
-    <div>
-      <div id="editor"></div>
-      <div>{md}</div>
-    </div>
+    <>
+      <div id="buttons">
+        <button onClick={downloadFile}>Save</button>
+        <input
+          type="file"
+          ref={inputRef}
+          style={{ display: "none" }}
+          onChange={handleFileOpen}
+        />
+        <button onClick={() => inputRef.current.click()}>Open file</button>
+        {fileName && <p>File name: {fileName}</p>}
+      </div>
+      <div id="app">
+        <div id="editor"></div>
+        <div id="previewer">{md}</div>
+      </div>
+    </>
   );
 }
 
